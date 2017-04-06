31950dee685ca3965b6b629eb86fbdf2dce944f9
Gaevoy Roman <Gaevoyrs@gmail.com>
2017-04-06 22:25:33 +0300

    Работа над поддержкой нескольких приложений расширяемостью

--- core/components/simpleView/component.php
+++ core/components/simpleView/component.php
@@ -25,4 +25,55 @@ class component
      */
     const NAME  =   'simpleView';
 
+    /**
+     * @var string шаблон
+     */
+    private $template = '';
+
+    /**
+     * @var array данные
+     */
+    private $data   =   Array();
+
+    /**
+     * @var string результат
+     */
+    private $result   =   '';
+
+
+    /**
+     * Устанавливает шаблон
+     * @param string $template шаблон
+     */
+    public function setTemplate($template)
+    {
+        $this->template =   $template;
+    }
+
+    /**
+     * Устанавливает Данные
+     * @param array $data Данные
+     */
+    public function setData(array $data = Array())
+    {
+        $this->data =   $data;
+    }
+
+    /**
+     * Рендерит данные
+     */
+    public function render()
+    {
+        $this->result   =   render::run($this->template, $this->data);
+    }
+
+    /**
+     * Отдает результат
+     * @return string результат
+     */
+    public function get()
+    {
+        return $this->result;
+    }
+
 }
