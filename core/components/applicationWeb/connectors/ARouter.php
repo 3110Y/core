69cfe89e2a3d165d12e17cad6c0395713c07f1a0
Gaevoy Roman <Gaevoyrs@gmail.com>
2017-04-09 23:59:35 +0300

    Работа над относительностью, гибкостью и обработкой ошибок

--- core/components/applicationWeb/connectors/ARouter.php
+++ core/components/applicationWeb/connectors/ARouter.php
@@ -41,30 +41,14 @@ abstract class ARouter
      */
     public function getSelectedPage($parent_id = 0)
     {
-        echo "<hr>";
-        echo "parent_id {$parent_id}<br>";
-
         $pageError  =   Array();
         $countURL  = count($this->url) - 1;
         foreach ($this->structure  as $item) {
-
-            $i = $parent_id + 1;
-            echo " {$countURL} === {$i}<br>";
-            echo "i {$i}<br>";
-            echo '<pre>';
-            print_r($this->url);
-            echo '</pre>';
-            echo '<br>';
-
             if ($countURL === $parent_id + 1 && isset($this->url[$parent_id + 1])) {
-                echo '№1';
-                echo "item['url'] {$item['url']}";
-                var_dump($this->url[$parent_id + 1]);
                 if ($item['url'] === $this->url[$parent_id + 1] && $item['parent_id'] === $parent_id) {
                     return $item;
                 }
             } else {
-                echo '№2';
                 if ($item['url'] === $this->url[$parent_id + 1] && $item['parent_id'] === $parent_id) {
                     return $this->getSelectedPage(++$parent_id);
                 }
