38c396a7fbbc5db3204873480a1d81067ce72914
Gaevoy Roman <gaevoy@indins.ru>
2017-04-21 11:39:16 +0300

    делаю драйвер дб

--- core/components/database/connectors/ADatabase.php
+++ core/components/database/connectors/ADatabase.php
@@ -89,7 +89,9 @@ class ADatabase
             $table  =   array_change_key_case($table, CASE_LOWER );
             $array  =   Array();
             foreach ($table as $key =>  $value) {
-                if (is_int($key)    &&  is_string($value)) {
+                if (is_int($key) && $value === ',') {
+                    $array[]    =   ",";
+                } elseif (is_int($key)    &&  is_string($value)) {
                     $array[]    =   "`{$value}`";
                 } elseif (is_string($key)    &&  is_string($value)) {
                     $array[]    =   "`$key` `{$value}`";
@@ -136,9 +138,13 @@ class ADatabase
                     } elseif (isset($value[1])) {
                         $t['t'] =   $value[1];
                     }
-
+                    $string   =     ($t['t'] != null)   ?   "`{$t['t']}` "   :   '';
+                    $string   .=    ($t['a'] != null)   ?   " `{$t['a']}`"   :   '';
+                    $string   .=    ($t['o'] != null)   ?   " {$t['o']} "    :   '';
+                    $array[]  =     $string;
                 }
             }
+            $table = implode(',', $array);
         } else {
             $table = "`{$table}`";
         }
@@ -161,44 +167,48 @@ class ADatabase
             $i=0;
             foreach ($where as $key => $value) {
                 if($i%2 && (!is_int($key) || is_array($value))) {
-                    $where['where'] .= ' AND ';
+                    $result['where'] .= ' AND ';
                     $i++;
                 }
                 if(is_int($key) && is_array($value)) {
                     $tmp_where = $this->where($value);
-                    $where['execute']   =       array_merge($where['execute'], $tmp_where['execute']);
-                    $where['where']     .=      "({$tmp_where['sql']})";
+                    $result['execute']   =       array_merge($result['execute'], $tmp_where['execute']);
+                    $result['where']     .=      "({$tmp_where['sql']})";
                 } elseif(is_int($key) && !is_array($value)) {
                     $where['where'] .= " {$value } ";
                 } elseif(is_array($value)) {
-                    $value['condition']             =   (isset($value['condition']))   ?   $value['condition']        :   '=';
-                    $prefix                         =   (isset($value['prefix']))       ?   " `{$value['prefix']}`."  :   '';
-                    if(!is_array($value['value']))
-                    {
-                        $where['sql']                   .=  $prefix . '`'.$key.'` '.(($value['condition'] == 'IN') ?   $value['condition'].' (:'.$key.')'  :   $value['condition'].' :'.$key);
-                        $where['execute'][':'.$key]     =   $value['value'];
+                    if (!isset($value['condition']) && isset($value['c'])) {
+                        $value['condition'] =   $value['c'];
                     }
-                    elseif(is_array($value['value']) && $value['condition']  == 'IN' )
-                    {
+                    if (!isset($value['value']) && isset($value['v'])) {
+                        $value['value'] =   $value['v'];
+                    }
+                    if (!isset($value['prefix']) && isset($value['p'])) {
+                        $value['prefix'] =   $value['p'];
+                    }
+                    if (isset($value['f'])) {
+                            $key =   $value['f'];
+                    }
+                    if (isset($value['field'])) {
+                            $key =   $value['field'];
+                    }
+                    $value['condition']             =   (isset($value['condition']))    ?   $value['condition']        :   '=';
+                    $prefix                         =   (isset($value['prefix']))       ?   " `{$value['prefix']}`."  :   '';
+                    if (!is_array($value['value'])) {
+                        $result['where']                .=  $prefix . '`'.$key.'` '.(($value['condition'] == 'IN') ?   $value['condition'].' (:'.$key.')'  :   $value['condition'].' :'.$key);
+                        $result['execute'][':'.$key]     =   $value['value'];
+                    } elseif(is_array($value['value']) && $value['condition']  == 'IN' ) {
                         $keyArray = Array();
-                        for($i=0;$i<count($value['value']);$i++)
-                        {
-                            $where['execute'][':'.$key.$i]     =   $value['value'][$i];
+                        for ($i=0;$i<count($value['value']);$i++) {
+                            $result['execute'][':'.$key.$i]     =   $value['value'][$i];
                             $keyArray[]   = ':'.$key.$i;
                         }
-                        $where['sql']                   .= $prefix.'`'.$key.'` '.$value['condition'].' ('.implode(",",$keyArray).')';
+                        $result['where']                   .= $prefix.'`'.$key.'` '.$value['condition'].' ('.implode(",",$keyArray).')';
                     }
                 } else {
-                    $where['where']                   .=  '`'.$key.'` = :'.$key;
-                    $where['execute'][':'.$key]     =   $value;
+                    $result['where']                   .=  '`'.$key.'` = :'.$key;
+                    $result['execute'][':'.$key]     =   $value;
                 }
-
-
-
-
-
-
-
                 $i++;
             }
         }
@@ -213,6 +223,59 @@ class ADatabase
      */
     protected function order($order)
     {
+        if ($order === null) {
+            $order = null;
+        } elseif (is_array($order)) {
+            $array  =   array();
+            foreach ($order as $key => $value) {
+                if (is_int($key) && !is_array($value)) {
+                    $array[]    =   $value;
+                } elseif (is_string($key) && !is_array($value)) {
+                    $array[]    =   "`{$key}` `{$value}`";
+                } elseif (is_array($value)) {
+                    $o  =   array(
+                        'f' => null,
+                        'd' => null,
+                        'a' => null,
+                    );
+                    $value  =   array_change_key_case($value, CASE_LOWER );
+                    if (isset($value['f'])) {
+                        $o['f'] =   $value['f'];
+                    } elseif (isset($value['fields'])) {
+                        $o['f'] =   $value['fields'];
+                    } elseif (is_string($key)) {
+                        $o['f'] =   $key;
+                    } elseif (isset($value[0])) {
+                        $o['f'] =   $key;
+                    }
+                    if (isset($value['d'])) {
+                        $o['d'] =   $value['d'];
+                    } elseif (isset($value['direction'])) {
+                        $o['d'] =   $value['direction'];
+                    } elseif (isset($value[1])) {
+                        $o['d'] =   $key;
+                    }
+                    if (isset($value['a'])) {
+                        $o['a'] =   $value['a'];
+                    } elseif (isset($value['as'])) {
+                        $o['a'] =   $value['as'];
+                    } elseif (isset($value['associate'])) {
+                        $o['a'] =   $value['associate'];
+                    } elseif (isset($value['alias'])) {
+                        $o['a'] =   $value['alias'];
+                    } elseif (isset($value['t'])) {
+                        $o['a'] =   $value['t'];
+                    } elseif (isset($value['table'])) {
+                        $o['a'] =   $value['table'];
+                    }
+                    $string   =     ($o['a'] != null)   ?   "`{$o['a']}` . "        :   '';
+                    $string   .=    ($o['f'] != null)   ?   "`{$o['f']}`"           :   '';
+                    $string   .=    ($o['d'] != null)   ?   " `{$o['d']}` "         :   '';
+                    $array[]  =     $string;
+                }
+            }
+            $order = implode(',', $array);
+        }
         return $order;
     }
 
@@ -223,6 +286,30 @@ class ADatabase
      */
     protected function limit($limit)
     {
+        if ($limit === null) {
+            $limit = null;
+        } elseif (is_array($limit)) {
+            $l = Array(
+                'f' => 0,
+                't' => 30,
+            );
+            $limit  =   array_change_key_case($limit, CASE_LOWER );
+            if (isset($limit['f'])) {
+                $l['f'] =   $limit['f'];
+            } elseif (isset($limit['from'])) {
+                $l['f'] =   $limit['from'];
+            } elseif (isset($limit[0])) {
+                $l['f'] =   $limit[0];
+            }
+            if (isset($limit['t'])) {
+                $l['t'] =   $limit['t'];
+            } elseif (isset($limit['to'])) {
+                $l['f'] =   $limit['to'];
+            } elseif (isset($limit[1])) {
+                $l['f'] =   $limit[1];
+            }
+            $limit = implode(',', $l);
+        }
         return $limit;
     }
 
@@ -233,6 +320,47 @@ class ADatabase
      */
     protected function group($group)
     {
+        if ($group === null) {
+            $group = null;
+        } elseif (is_array($group)) {
+            $array  =   array();
+            foreach ($group as $key => $value) {
+                if (is_int($key) && !is_array($value)) {
+                    $array[]    =   $value;
+                } elseif (is_string($key) && !is_array($value)) {
+                    $array[]    =   "`{$key}` . `{$value}`";
+                } elseif (is_array($value)) {
+                    $g  =   array(
+                        't' => null,
+                        'v' => null,
+                    );
+                    $value  =   array_change_key_case($value, CASE_LOWER );
+                    if (isset($value['t'])) {
+                        $g['t'] =   $value['t'];
+                    } elseif (isset($value['table'])) {
+                        $g['t'] =   $value['table'];
+                    } elseif (is_string($key)) {
+                        $g['t'] =   $key;
+                    }
+                    if (isset($value['v'])) {
+                        $g['v'] =   $value['v'];
+                    } elseif (isset($value['value'])) {
+                        $g['v'] =   $value['value'];
+                    }  elseif (isset($value['val'])) {
+                        $g['v'] =   $value['val'];
+                    } elseif (isset($value['f'])) {
+                        $g['v'] =   $value['f'];
+                    } elseif (isset($value['field'])) {
+                        $g['v'] =   $value['field'];
+                    }
+                    $string   =     ($g['t'] != null)   ?   "`{$g['t']}` . "   :   '';
+                    $string   .=    ($g['v'] != null)   ?   "`{$g['v']}`"   :   '';
+                    $string   .=    ($g['a'] != null)   ?   " AS `{$g['v']}` "   :   '';
+                    $array[]  =     $string;
+                }
+            }
+            $group = implode(',', $array);
+        }
         return $group;
     }
 
