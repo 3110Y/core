06d7b7bfb86489abb7183bf48a27923e7a69bea3
Gaevoy Roman <gaevoy@indins.ru>
2017-06-21 15:20:30 +0300

    + компонент файл кэша

---
+++ core/component/fileCache/fileCache.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roman
+ * Date: 21.6.2017
+ * Time: 15:17
+ */
+
+namespace core\component\fileCache;
+
+
+/**
+ * Class fileCache
+ *
+ * @package core\component\fileCache
+ */
+class fileCache
+{
+
+}
