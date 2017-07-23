0f3e3994615c173135140eb018e9300e0ee31c62
Gaevoy Roman <Gaevoyrs@gmail.com>
2017-07-23 22:24:26 +0300

    Библиотека вендоров

---
+++ core/component/library/component.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaevoy
+ * Date: 23.07.17
+ * Time: 22:23
+ */
+
+namespace core\component\library;
+
+
+class component
+{
+    /**
+     * @const float Версия
+     */
+    const VERSION   =   1.0;
+
+}
