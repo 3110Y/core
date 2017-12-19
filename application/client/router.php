d7be2b4ecce7aa2f13246a33a3afc52cd31617bb
Gaevoy Roman <gaevoyrs@gmail.com>
2017-12-19 23:40:50 +0300

    переработка cform

--- application/client/router.php
+++ application/client/router.php
@@ -66,7 +66,8 @@ final class router extends applicationWeb\ARouter implements applicationWeb\IRou
         }
 
         self::selectPage();
-        $controllerBasic    =   new controllers\basic();
+        $controllerBasic    =   'application\\' . self::$application['path'] . '\controllers\\' . self::$application['basicController'];
+        $controllerBasic    =   new $controllerBasic();
         if ($controllerBasic instanceof applicationWeb\IControllerBasic) {
             $controllerBasic->preInit();
         }
