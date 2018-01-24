e0c4beba4a9daa660c096f6977484e7f40ab367f
Gaevoy Roman <gaevoyrs@gmail.com>
2018-01-24 12:22:09 +0300

    hotfix

--- core/core.php
+++ core/core.php
@@ -24,7 +24,7 @@ final class core
      * 4 версия исправления ошибок или предыдущего
      * 5 версия хотфиксов
      */
-    const VERSION   =   1.9091;
+    const VERSION   =   1.9092;
     
     /**
      * @const string Путь до компонентов
