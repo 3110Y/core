c6aa692f1ca2ebad9f85449215bc0815a16e395e
Dmitry Kirillov <veshnev@gmail.com>
2018-02-05 10:59:43 +0300

    FIX BUG: Изменение области видимости для $template в basic контроллере

--- application/client/controllers/basic.php
+++ application/client/controllers/basic.php
@@ -28,7 +28,7 @@ class basic extends application\AControllers implements application\IControllers
     /**
      * @var string шаблон
      */
-    protected $template = 'basic';
+    public $template = 'basic';
 
     /**
      * Преинициализация
